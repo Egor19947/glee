c182887a69fa4ea52839a6f7bd17b76378cd24c7
Egor19947 <egor.kulikoff@mail.ru>
2021-03-26 17:44:00 +0300

    added main filter

--- app/js/main.js
+++ app/js/main.js
@@ -1,10 +1,31 @@
 $(function(){
 
+  $('.filter-price__input').ionRangeSlider({
+    type: 'double',
+    step: 0.01,
+    onStart: function (data){
+      $('.filter-price__from').text(data.from);
+      $('.filter-price__to').text(data.to);
+    },
+    onChange: function (data){
+      $('.filter-price__from').text(data.from);
+      $('.filter-price__to').text(data.to);
+    }
+  });
+
   $('.slider-top__inner').slick({
     dots: true,
     arrows: false,
     fade: true
   });
+
+  $(".stars").rateYo({
+    starWidth: "11px",
+    normalFill: "#d6d6d6",
+    ratedFill: "#ffcc00",
+    readOnly: true
+  });
+
 
 
 
